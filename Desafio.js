b523cd228bd0cdb9b6b744228ec464e2a97b60ce
miguelsouzalima <miguelsouzalima99@gmail.com>
2024-09-15 14:19:05 -0300

    Criando o Laço de Repetição

--- Desafio.js
+++ Desafio.js
@@ -1,3 +1,13 @@
 // Criação das Variaveis em Array
 
 let heroi = [{nome: "Miguel", xp: 10500}]
+
+// ----- ~~~*Criando o Laço de Repetição
+for (let repetiçao = 0; repetiçao < heroi.length; repetiçao ++)
+    
+   {
+       
+    let nomeHeroi = heroi[repetiçao].nome
+    let xpHeroi = heroi[repetiçao].xp
+    let tier = ''
+   
